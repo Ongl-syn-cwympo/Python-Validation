9d6615e1bf121459861929331355877cbee998a6
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-24 15:52:09 +0000

    Create Range Check.py

---
+++ Range Check.py
@@ -0,0 +1,19 @@
+# Checks for a set range - Used age as example here
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+print("Enter your age")
+
+AGE = int(input())
+if AGE < 13:
+  print("Too young - Get out!")
+elif AGE == 69:
+    print("Damn good age, welcome aboard ")
+elif AGE > 99:
+  print("too old - Please leave.")
+else:
+    print("Welcome! ")
+
+
+
